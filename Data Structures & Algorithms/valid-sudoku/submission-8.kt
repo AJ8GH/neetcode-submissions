619d523963fc32b12a00103e2bc49e0b6dbdcd74
Adam Jonas <76049802+AJ8GH@users.noreply.github.com>
2026-04-19 09:20:03 +0100

    Add: valid-sudoku - submission-8

---
+++ Data Structures & Algorithms/valid-sudoku/submission-8.kt
@@ -0,0 +1,25 @@
+const val EMPTY = '.'
+
+class Solution {
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+          val rows = Array(board.size) { BooleanArray(board.size) }
+  val cols = Array(board.size) { BooleanArray(board.size) }
+  val squares = Array(board.size) { BooleanArray(board.size) }
+  for (i in board.indices) {
+    for (j in board[i].indices) {
+      val c = board[i][j]
+      if (c == EMPTY) continue
+      val idx = c - '1'
+      if (rows[i][idx]) return false
+      rows[i][idx] = true
+      if (cols[j][idx]) return false
+      cols[j][idx] = true
+      val key = (i / 3) * 3 + (j / 3)
+      if (squares[key][idx]) return false
+      squares[key][idx] = true
+    }
+  }
+  return true
+
+    }
+}
