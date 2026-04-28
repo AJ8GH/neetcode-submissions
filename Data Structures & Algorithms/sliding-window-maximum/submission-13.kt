2911473f98f3b2d8f63dc9ad95173d0871af5f73
Adam Jonas <76049802+AJ8GH@users.noreply.github.com>
2026-04-28 23:22:39 +0100

    Add: sliding-window-maximum - submission-13

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-13.kt
@@ -0,0 +1,23 @@
+import kotlin.Int.Companion.MAX_VALUE
+
+class Solution {
+fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {  
+    val maxes = IntArray((nums.size - k) + 1)
+  val deque = ArrayDeque<Int>()
+  for ((i, n) in nums.withIndex()) {
+    while (deque.isNotEmpty() && deque.first < n) {
+      deque.removeFirst()
+    }
+    if (deque.isNotEmpty() && i >= k && nums[i - k] == deque.last) {
+      deque.removeLast()
+    }
+    if (deque.isNotEmpty() && n > deque.last) deque.addLast(n)
+    else deque.addFirst(n)
+    if (i >= k - 1) {
+      maxes[(i - k) + 1] = deque.last
+    }
+  }
+  return maxes
+}
+
+}
