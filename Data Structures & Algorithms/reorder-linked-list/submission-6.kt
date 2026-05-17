9c7aaa2400cee81aac525b02eea8c225e0e0387e
Adam Jonas <76049802+AJ8GH@users.noreply.github.com>
2026-05-17 12:19:05 +0100

    Add: reorder-linked-list - submission-6

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-6.kt
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+fun reorderList(head: ListNode?) {
+  head ?: return
+  var first = head
+  var second = split(head)
+  second = reverse(second)
+  while (first != null) {
+    val tmp = first.next
+    first.next = second
+    first = second
+    second = tmp
+  }
+}
+
+private fun split(head: ListNode): ListNode? {
+  var slow: ListNode? = head
+  var fast: ListNode? = head.next
+  while (fast?.next != null) {
+    fast = fast.next?.next
+    slow = slow?.next
+  }
+  val ret = slow?.next
+  slow?.next = null
+  return ret
+}
+
+@Suppress("DuplicatedCode")
+private fun reverse(second: ListNode?): ListNode? {
+  var current: ListNode?
+  current = second
+  var next = current?.next
+  current?.next = null
+  while (next != null) {
+    val tmp = next.next
+    next.next = current
+    current = next
+    next = tmp
+  }
+  return current
+}
+
+}
